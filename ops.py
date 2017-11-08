d730ad8cace77b3e31f31e7a79112f68c35ca1c9
Andrew <andrewliao11@gmail.com>
2017-11-08 10:03:44 -0800

    Create ops.py

---
+++ ops.py
@@ -0,0 +1,13 @@
+# build your own tensorflow wrapper
+# trust me. it will help you a lot when you want to 
+# change the behaviors of some operations
+import tensorflow as tf
+
+def conv2d:
+  ...
+  
+def fc:
+  ...
+
+def deconv2d:
+  ...
